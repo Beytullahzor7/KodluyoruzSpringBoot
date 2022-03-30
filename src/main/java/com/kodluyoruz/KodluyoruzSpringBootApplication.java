e40375a6cdb89223f54fcab39f5e87ce91566e07
Beytullahzor7 <beytullahzor7@gmail.com>
2022-03-30 22:18:29 +0300

    Config Completed

--- src/main/java/com/kodluyoruz/KodluyoruzSpringBootApplication.java
+++ src/main/java/com/kodluyoruz/KodluyoruzSpringBootApplication.java
@@ -3,11 +3,12 @@ package com.kodluyoruz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+       org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class KodluyoruzSpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KodluyoruzSpringBootApplication.class, args);
     }
-
 }
